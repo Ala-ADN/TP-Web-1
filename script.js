045bdc23aacd6a7b38517b27498e78d52adde593
Ala-ADN <alaadnzaw@gmail.com>
2024-02-15 14:33:39 +0100

    through out event listeners I alone am the deaf one

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function random(max){
     return Math.floor(1+Math.random()*(max-1))
 }
-let difficulty = document.querySelector("#Difficulty");
-console.log(difficulty)
+let diffSelector = document.querySelector("#difficulty")
+let range = document.querySelector(".range")
+let input = document.querySelector("input")
+let tries = document.querySelector(".tries")
+
+var value
+
+function start(){
+    let s = diffSelector.getAttribute("selectedIndex")
+    if(s==0){
+        value = random(10)
+        range.innerText = 1-10
+        tries.innerText = 4
+    }
+    if(s==1){
+        value = random(50)
+        range.innerText = 1-50
+        tries.innerText = 7
+    }
+    if(s==1){
+        value = random(100)
+        range.innerText = 1-100
+        tries.innerText = 10
+    }
+
+}
