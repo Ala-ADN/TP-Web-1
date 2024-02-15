133f702747bf0d9e94cfc53116b0ca7309a70540
Your Name <you@example.com>
2024-02-15 14:54:13 +0100

    besbes last code

--- script.js
+++ script.js
@@ -1,58 +1,79 @@
-const selectElement= document.getElementById("difficulty");
-let state="Easy";
-let x=5;
+const selectDifficulty= document.getElementById("difficulty");
 const selectBtn = document.getElementById("btn");
+const inputElement=document.getElementById("input");
+
+
+let state="Easy";
+let numberGuess=5;
 
 
 
-selectElement.addEventListener('change',() =>{
-  const selectedIndex = selectElement.selectedIndex;
-    let selectLabel = document.getElementById("range");
 
-    state = selectElement.options[selectedIndex].value;
+function generateNumber(state){
+  let randomValue;
   if (state==="Easy"){
-    x=Math.floor(Math.random()*10);
+    randomValue=Math.floor(Math.random()*10);
+  }
+  else if (state==="Medium"){
+    randomValue=Math.floor(Math.random()*30);
+  }
+  else if (state==="Hard"){
+    randomValue=Math.floor(Math.random()*50);
+  }
+  return randomValue;
+}
+function editIntervalLabel(state){
+  let selectLabel = document.getElementById("range");
+  if (state==="Easy"){
     selectLabel.textContent="Interval: 0 - 10";
   }
   else if (state==="Medium"){
-      x=Math.floor(Math.random()*30);
     selectLabel.textContent="Interval: 0 - 30";
-
   }
   else if (state==="Hard"){
-      x=Math.floor(Math.random()*50);
     selectLabel.textContent="Interval: 0 - 50";
-
   }
+}
+
+selectDifficulty.addEventListener("change",() =>{
+  const selectedIndex = selectDifficulty.selectedIndex;
+  state = selectDifficulty.options[selectedIndex].value;
+  numberGuess=generateNumber(state);
+  editIntervalLabel(state);
 }
 )
 
-let inputElement=document.getElementById("input");
-inputElement.addEventListener("keypress",(e)=>{
+/*inputElement.addEventListener("keypress",(e)=>{
   if(e.key==="Enter"){
     let inputValue=parseInt(inputElement.value);
-    if (inputValue===x){
+    if (inputValue===numberGuess){
       alert("You won!");
     }
     else {
-      if (inputValue<x)
+      if (inputValue<numberGuess)
         alert("Your input is smaller than the target");
       else
         alert("Your input is bigger than the target");
     }
   }
-})
+})*/
+
+let inputValue=-1;
+while(inputValue!==numberGuess){
+  inputValue=parseInt(prompt("Guess the number"));
+  if (inputValue===numberGuess){
+    alert("You won!");
+  }
+  else {
+    if (inputValue<numberGuess)
+      alert("Your input is smaller than the target");
+    else
+      alert("Your input is bigger than the target");
+  }
+}
 
 selectBtn.addEventListener("click",()=>{
-  if (state==="Easy"){
-    x=Math.floor(Math.random()*10);
-  }
-  else if (state==="Medium"){
-    x=Math.floor(Math.random()*30);
-  }
-  else if (state==="Hard"){
-    x=Math.floor(Math.random()*50);
-  }
+  numberGuess=generateNumber(state);
   alert("A new X has been generated." +
     "Play Again!");
   inputElement.value="";
